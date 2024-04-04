951e6ba6854bbd4c1f09fe1cdbe07757e68ab85b
DiegoAndresM <65470307+DiegoAndresM@users.noreply.github.com>
2024-04-03 18:36:19 -0600

    db update

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = "mongodb+srv://DiegoM:Diego2003@cluster0.a456lns.mongodb.net/" //Poner aca la uri
+const mongoUrl = "" //Poner aca la uri
 // const mongoUrl = process.env.MONGO_URL
 
 // console.log('uy: ', mongoUrl)
